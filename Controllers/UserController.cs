de5b6ad59a585e6a52254b5546f58e91aae6de36
Boitshoko Mphahlele <boitshokontshoekgolo@gmail.com>
2025-07-07 11:19:00 +0200

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using LearninngManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearninngManagementSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult GuestServiceView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult BookingView(Booking booking)
+        {
+
+        }
+        public ActionResult BookingView() {
+            return View();
+        }
+    }
+}
---
+++ Models/Booking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearninngManagementSystem.Models
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public string P_FullName { get; set; }
+        public string L_FullName {  get; set; }
+        public int Grade {  get; set; }
+        public string P_PhoneNumber { get; set; }
+        public string L_PhoneNumber { get; set; }
+        public DateTime BookingDate { get; set; }
+        public DateTime DateMade { get; set; }
+        public string Home_Language { get; set; }
+
+        public Booking()
+        {
+
+        }
+
+    }
+}
